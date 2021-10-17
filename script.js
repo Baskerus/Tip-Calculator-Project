ec16af1b6e383c01422b6bd66019db7ab1ccc52a
Baskerus <baskerus@gmail.com>
2021-10-17 15:03:42 +0200

    calculator functionality finished

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const resetBtn = document.getElementById("reset");
 const bill = document.getElementById("bill");
 const numOfPeople = document.getElementById("ppl-num");
-const custom = document.getElementById("custom");
+const custom = document.querySelector(".custom");
 const fields = [bill, custom, numOfPeople];
 const tips = Array.from(document.querySelectorAll(".tip-option"));
 const tipDisplay = document.querySelector(".tip-display");
@@ -19,11 +19,15 @@ resetBtn.addEventListener("click", reset);
 tips.forEach((tip) => {
   tip.addEventListener("click", () => {
     tips.forEach((tip) => {
+      isCustomTip = false;
+      tip.classList.remove("active-custom");
       tip.id = "";
     });
     tip.id = "selected";
 
     if (tip.classList.contains("custom")) {
+      isCustomTip = true;
+      tip.classList.add("active-custom");
       currentTipPercent = parseInt(tip.value, 10);
     } else {
       currentTipPercent = tip.innerText.substring(0, tip.innerText.length - 1);
@@ -44,6 +48,8 @@ fields.forEach((element) => {
   });
 });
 
+custom.addEventListener("input", calculateAndDisplay);
+
 /* ========= Functions ========= */
 
 function reset() {
@@ -60,14 +66,20 @@ function reset() {
 }
 
 function calculateAndDisplay() {
-  if ((isCustomTip = true)) {
-    currentTipPercent = parseInt(custom.value);
-  } else {
-    let billTotal = parseInt(bill.value);
-    let tipPerPerson = (billTotal / 100) * currentTipPercent;
-    let totalPerPerson = (billTotal / 100) * currentTipPercent + billTotal;
+  let billTotal = parseFloat(bill.value);
+  let tipPerPerson = (billTotal / 100) * currentTipPercent;
+  let totalPerPerson = (billTotal / 100) * currentTipPercent + billTotal;
 
-    totalDisplay.innerText = totalPerPerson / numOfPeople.value;
-    tipDisplay.innerText = tipPerPerson / numOfPeople.value;
+  if (isNaN(billTotal) || isNaN(currentTipPercent)) {
+    tipDisplay.innerText = "$0.00";
+    totalDisplay.innerText = "$0.00";
+  } else {
+    tipDisplay.innerText = "$" + (tipPerPerson / numOfPeople.value).toFixed(2);
+    totalDisplay.innerText =
+      "$" + (totalPerPerson / numOfPeople.value).toFixed(2);
+  }
+
+  if (isCustomTip) {
+    currentTipPercent = parseInt(custom.value);
   }
 }
